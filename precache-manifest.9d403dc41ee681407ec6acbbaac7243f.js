5a078f608b60e84185c5cb497fb9124ff32ac49c
bry <bry@MacBook-Pro-bry.local>
2021-01-29 16:27:25 +0700

    first commit

---
+++ precache-manifest.9d403dc41ee681407ec6acbbaac7243f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e9715fa510fa663feaf888b5a2596a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cb77f2e748083ff4f6c",
+    "url": "/static/css/main.8a870812.chunk.css"
+  },
+  {
+    "revision": "7a7e032e636737f61fb8",
+    "url": "/static/js/2.9e2a3b38.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9e2a3b38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cb77f2e748083ff4f6c",
+    "url": "/static/js/main.9d25747a.chunk.js"
+  },
+  {
+    "revision": "2ed230d235986e42ce91",
+    "url": "/static/js/runtime-main.8dc6e08f.js"
+  },
+  {
+    "revision": "f69eac02bb9c2803e00f7c9c6b06b90a",
+    "url": "/static/media/Untitled-1.f69eac02.png"
+  },
+  {
+    "revision": "48837b86e4347692917d55059f0f249a",
+    "url": "/static/media/bg-main-01.48837b86.png"
+  },
+  {
+    "revision": "bf349ca2ae7aed6b537e9d6177a70258",
+    "url": "/static/media/contact.bf349ca2.png"
+  },
+  {
+    "revision": "37fe8322b169ddbdeabf75930e886ac6",
+    "url": "/static/media/react.37fe8322.png"
+  }
+]);
